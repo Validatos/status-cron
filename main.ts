3130bbbee198290e64abcf06c56e55c02f217f8b
Adrian Covarrubias <adriancova.95@gmail.com>
2023-11-16 12:55:38 -0800

    deno formatter

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
-const validatosStatusApiUrl = Deno.env.get('VALIDATOS_STATUS_API_URL');
+const validatosStatusApiUrl = Deno.env.get("VALIDATOS_STATUS_API_URL");
 const fetchServiceStatus = async () => {
   if (!validatosStatusApiUrl) {
-    console.log('status url not set!');
+    console.log("status url not set!");
     return;
   }
   const res = await fetch(validatosStatusApiUrl);
   const jsonStatus = await res.json();
-  console.log('obtained services status: ', jsonStatus);
+  console.log("obtained services status: ", jsonStatus);
 };
 
-Deno.cron('check-status-validatos', '*/10 * * * * *', fetchServiceStatus);
+Deno.cron("check-status-validatos", "*/10 * * * * *", fetchServiceStatus);
