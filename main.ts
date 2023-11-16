2781928d4d557b6f9e7a92dfa1bcf26c9bfe0edf
Adrian Covarrubias <adriancova.95@gmail.com>
2023-11-16 12:54:58 -0800

    cron job with fetchServiceStatus

---
+++ main.ts
@@ -0,0 +1,12 @@
+const validatosStatusApiUrl = Deno.env.get('VALIDATOS_STATUS_API_URL');
+const fetchServiceStatus = async () => {
+  if (!validatosStatusApiUrl) {
+    console.log('status url not set!');
+    return;
+  }
+  const res = await fetch(validatosStatusApiUrl);
+  const jsonStatus = await res.json();
+  console.log('obtained services status: ', jsonStatus);
+};
+
+Deno.cron('check-status-validatos', '*/10 * * * * *', fetchServiceStatus);
